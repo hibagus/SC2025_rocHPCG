851ba9df7f9135ea81e759c1993d21fb6c277801
Bagus Hanindhito <git@bagus.my.id>
2025-08-25 15:20:02 +0000

    modify batch script

--- build/batch/run_batch_single.sh
+++ build/batch/run_batch_single.sh
@@ -1,4 +1,4 @@
 ./run_workload_monitor_single.sh "./amd_mi300x_power_clock_temp_single.sh 0 1 run_HPCG_Single_624"  "./amd_mi300x_hbm3_single.sh 0 1 run_HPCG_Single_624" "./run_single_GPU.sh 624"
 ./run_workload_monitor_single.sh "./amd_mi300x_power_clock_temp_single.sh 0 1 run_HPCG_Single_560"  "./amd_mi300x_hbm3_single.sh 0 1 run_HPCG_Single_560" "./run_single_GPU.sh 560"
 ./run_workload_monitor_single.sh "./amd_mi300x_power_clock_temp_single.sh 0 1 run_HPCG_Single_280"  "./amd_mi300x_hbm3_single.sh 0 1 run_HPCG_Single_280" "./run_single_GPU.sh 280"
-./run_workload_monitor_single.sh "./amd_mi300x_power_clock_temp_single.sh 0 1 run_HPCG_Single_140"  "./amd_mi300x_hbm3_single.sh 0 1 run_HPCG_Single_140" "./run_single_GPU.sh 120"
+#./run_workload_monitor_single.sh "./amd_mi300x_power_clock_temp_single.sh 0 1 run_HPCG_Single_120"  "./amd_mi300x_hbm3_single.sh 0 1 run_HPCG_Single_120" "./run_single_GPU.sh 120"
